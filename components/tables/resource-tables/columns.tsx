82c6af42940bc6f7134021b2661298f193892dbf
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-04 22:40:43 +0200

    Return node columns

--- components/tables/resource-tables/columns.tsx
+++ components/tables/resource-tables/columns.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { NodeColumns } from "./columns/nodeColumns";
 import { ServiceColumns } from "./columns/serviceColumns";
 
 
 export function getColumns(resource:string){
   if (resource === "services") return ServiceColumns
+  if (resource === "nodes") return NodeColumns
 }
