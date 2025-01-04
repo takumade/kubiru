89f2ae287daf58029ee35625072fc500146d1ca7
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-04 23:02:53 +0200

    Add pod columns

--- components/tables/resource-tables/columns.tsx
+++ components/tables/resource-tables/columns.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { NodeColumns } from "./columns/nodeColumns";
+import { PodColumns } from "./columns/podColumns";
 import { ServiceColumns } from "./columns/serviceColumns";
 
 
 export function getColumns(resource:string){
   if (resource === "services") return ServiceColumns
   if (resource === "nodes") return NodeColumns
+  if (resource === "pods") return PodColumns
 }
