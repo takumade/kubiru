bfe299a47c56c8ba982abe7dd4e910d1ae17d201
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-06 23:50:49 +0200

    Return formatted clolumns

--- components/tables/resource-tables/columns.tsx
+++ components/tables/resource-tables/columns.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { DeploymentColumns } from "./columns/deploymentColumns";
+import { NamespaceColumns } from "./columns/namespacesColumns";
 import { NodeColumns } from "./columns/nodeColumns";
 import { PodColumns } from "./columns/podColumns";
 import { ServiceColumns } from "./columns/serviceColumns";
@@ -11,4 +12,5 @@ export function getColumns(resource:string){
   if (resource === "nodes") return NodeColumns
   if (resource === "pods") return PodColumns
   if (resource === "deployments") return DeploymentColumns
+  if (resource === "namespaces") return NamespaceColumns
 }
