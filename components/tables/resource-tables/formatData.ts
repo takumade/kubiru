97e4d975e922bd6336299876f78e17625d63771d
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-06 23:41:48 +0200

    Return formatted data in formatData

--- components/tables/resource-tables/formatData.ts
+++ components/tables/resource-tables/formatData.ts
@@ -49,6 +49,7 @@ export function formatData(resource:string, data:any){
     if (resource == "services") return formatServices(data as ResourceItems[])
     if (resource == "nodes") return formatNodes(data as ResourceItems[])
     if (resource == "pods") return formatPods(data as ResourceItems[])
+    if (resource == "deployments") return formatDeployments(data as ResourceItems[])
     
 
 
