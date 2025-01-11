7ffcfbac82637c106735b8f5aaba76ae8c7cc9aa
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-11 23:56:32 +0200

    Chnange logo and image

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import UserAuthForm from '@/components/forms/user-auth-form';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Authentication',
@@ -23,7 +24,7 @@ export default function AuthenticationPage() {
       </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
+        <div  className="relative z-20 flex items-center text-lg font-medium">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -38,8 +39,22 @@ export default function AuthenticationPage() {
           </svg>
           Kubiru
         </div>
-        <div className="relative z-20 mt-auto">
+
+
+
+        
+        <div  className="relative z-20 mt-auto">
+       
           <blockquote className="space-y-2">
+
+          <Image
+    src="/images/k8.png"
+    alt="A kubernetes image"
+    width={200}
+    height={200}
+
+    className='mx-auto relative bottom-52'
+    />
             <p className="text-lg">
               &ldquo;This library has saved me countless hours of work and
               helped me deliver stunning designs to my clients faster than ever
