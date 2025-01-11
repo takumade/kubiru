75372abb38956258f1a137ec6a59cae50e1045aa
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-11 22:25:49 +0200

    Chane revenue to clusters

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -39,9 +39,9 @@ export default function page() {
               <Card>
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                   <CardTitle className="text-sm font-medium">
-                    Total Revenue
+                    Clusters
                   </CardTitle>
-                  <svg
+                  {/* <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="none"
@@ -52,10 +52,11 @@ export default function page() {
                     className="h-4 w-4 text-muted-foreground"
                   >
                     <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                  </svg>
+                  </svg> */}
+                  
                 </CardHeader>
                 <CardContent>
-                  <div className="text-2xl font-bold">$45,231.89</div>
+                  <div className="text-2xl font-bold">10</div>
                   <p className="text-xs text-muted-foreground">
                     +20.1% from last month
                   </p>
