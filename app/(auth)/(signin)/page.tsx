f1cc30ab4c188c4dccd45d74dd30a6a97bc509cb
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-11 23:07:32 +0200

    Change logo

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -36,7 +36,7 @@ export default function AuthenticationPage() {
           >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
-          Logo
+          Kubiru
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
