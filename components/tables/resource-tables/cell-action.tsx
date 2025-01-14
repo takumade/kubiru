a1d104f4011c7cff8b99dd994c3354fa63fc3531
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-14 23:10:52 +0200

    Change link address

--- components/tables/resource-tables/cell-action.tsx
+++ components/tables/resource-tables/cell-action.tsx
@@ -66,7 +66,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
 
           <DropdownMenuItem
-            onClick={() => router.push(`/dashboard/resources/${data.id}`)}
+            onClick={() => router.push(`/dashboard/resources/${data.original}`)}
           >
             <Edit className="mr-2 h-4 w-4" /> Update
           </DropdownMenuItem>
