113636edfdb19221f7feb792367c0a29b950ab7f
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-14 23:47:33 +0200

    get request details

--- repositories/k8Repository.ts
+++ repositories/k8Repository.ts
@@ -37,14 +37,29 @@ async function getNamespacesFromCookies(){
     }
 
 
-
-
-
-export async function getResource(resource:string, api_type: string="api_v1" ) {
+async function getRequestDetails(){
     let cluster: Cluster = await getClusterFromCookies()
     let namespace = await getNamespacesFromCookies()
 
     let headers = generateHeaders(cluster.token)
+
+    return {
+        cluster,
+        namespace,
+        headers
+    }
+}
+
+
+
+
+export async function getResource(resource:string, api_type: string="api_v1" ) {
+
+    const {
+        cluster,
+        namespace, 
+        headers
+    } = await getRequestDetails()
 
     console.log("headers: ", headers)
 
@@ -60,8 +75,53 @@ export async function getResource(resource:string, api_type: string="api_v1" ) {
         apiVersion = "api/v1"
     }
 
+    let resourceUrl = `${cluster.api}/${apiVersion}/${resource}`
+
+    if (namespace) {
+        resourceUrl = `${cluster.api}/${apiVersion}/namespaces/${namespace}/${resource}`
+    }
+
+    console.log("Resource URLs: ", resourceUrl)
+
+    let results = await fetch(resourceUrl, {
+        method: 'GET',
+        headers: headers,
+        agent: new https.Agent({
+            rejectUnauthorized: false,
+          })
+        
+    })
+
+    let data:any = await results.json()
+
+    console.log("Data x: ", data)
+
+    if (data.items) {
+        return data.items
+    }
+
+    return []
+}
+
+export async function getResourceDetails(resource:string, api_type: string="api_v1" ) {
+    const {
+        cluster,
+        namespace, 
+        headers
+    } = await getRequestDetails()
 
 
+    let apiVersion = api_type
+
+    if (api_type === "apps_v1" || resource === "deployments") {
+        apiVersion = "apis/apps/v1"
+    } else if (api_type === "batch_v1") {
+        apiVersion = "apis/batch/v1"
+    } else if (api_type === "extensions_v1beta1") {
+        apiVersion = "apis/extensions/v1beta1"
+    } else if (api_type === "api_v1") {
+        apiVersion = "api/v1"
+    }
 
     let resourceUrl = `${cluster.api}/${apiVersion}/${resource}`
 
@@ -104,29 +164,5 @@ export async function setCurrentNamespace(namespace: string) {
 }
 
 
-export async function getNodes() {
-    return await getResource('nodes')
-}
-
-export async function getServices() {
-    return await getResource('services')
-}
-
-
-export async function getPods() {
-    return await getResource('pods')
-}
-
-export async function getDeployments() {
-    return await getResource('deployments',  'apps_v1')
-}
-
-export async function getNamespaces() {
-    return await getResource('namespaces')
-}
-
-export async function getVolumes() {
-    return await getResource('persistentvolumes')
-}
 
 
